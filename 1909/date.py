0b14703b134a68e5fab082ecf0e9c86328854e83
kirillsev1 <kirillprih47@gmail.com>
2022-09-19 15:02:52 +0300

    HomeWork 1909

--- 1909/date.py
+++ 1909/date.py
@@ -1,12 +1,10 @@
 def ceck():
-    day = int(input('Введите день:'))
-    month = int(input('Введите месяц:'))
-    year = int(input('Введите год:'))
     try:
+        day = int(input('Введите день:'))
+        month = int(input('Введите месяц:'))
+        year = int(input('Введите год:'))
         calendar = [31, 28, 31, 30, 31, 31, 30, 31, 30, 31, 30, 31]
-        if year % 100 == 0:
-            pass
-        elif year % 4 == 0:
+        if year % 400 == 0 and year % 100 == 0 or year % 4 == 0 and year % 100 != 0:
             calendar.insert(1, 29)
             del calendar[3]
         if day >= 1 and day <= calendar[month - 1]:
